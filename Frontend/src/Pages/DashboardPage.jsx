6ea4ecd5215d935dca6f0a057ba77d54f0c4647a
Deependra Pratap Singh <145841117+Deependra0043@users.noreply.github.com>
2026-06-02 21:56:10 +0530

    Update DashboardPage.jsx

--- Frontend/src/Pages/DashboardPage.jsx
+++ Frontend/src/Pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router";
 import { useUser } from "@clerk/clerk-react";
 import { useState } from "react";
-import { useActiveSessions, useCreateSession, useMyRecentSessions } from "../Hooks/useSessions";
+import { useActiveSessions, useCreateSession, useMyRecentSessions } from "../Hooks/UseSessions";
 
 import Navbar from "../components/Navbar";
 import WelcomeSection from "../components/WelcomeSection";
